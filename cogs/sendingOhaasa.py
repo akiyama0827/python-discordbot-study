a6b4e68a1f04ab64b44bb6b1d5c287dab543664d
akiyama0827__ <93310538+akiyama0827@users.noreply.github.com>
2026-02-22 23:22:24 +0900

    1

--- cogs/sendingOhaasa.py
+++ cogs/sendingOhaasa.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import tasks, commands
 from datetime import datetime
 from dotenv import load_dotenv
+from pytz import timezone
 
 import crawllingOhaasa
 horo_text_list = crawllingOhaasa.horo_text_list
