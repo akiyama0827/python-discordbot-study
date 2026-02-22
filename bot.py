5af89d600893d061a073d7dc16b5344de89d7f5e
akiyama0827__ <93310538+akiyama0827@users.noreply.github.com>
2026-02-22 23:29:38 +0900

    1

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os, discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
+from pytz import timezone
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
