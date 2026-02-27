f33ac388b9fb9684cf3fe9f2e080e2887849606c
akiyama0827__ <93310538+akiyama0827@users.noreply.github.com>
2026-02-27 23:31:02 +0900

    .

--- data/build.sql
+++ data/build.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS player (
-    user_id text PRIMARY KEY,
-    enlistment_date text,
-    discharge_date text,
-    branch text,
+    user_id INTEGER PRIMARY KEY,
+    enlistment_date TEXT,
+    discharge_date TEXT,
+    branch TEXT
 );
