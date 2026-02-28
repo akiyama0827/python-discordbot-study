dd05168e4d289a2955f885a355a23932ccee6627
akiyama0827__ <93310538+akiyama0827@users.noreply.github.com>
2026-02-28 23:11:23 +0900

    .

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ async def load_extensions():
     for filename in os.listdir('cogs'):
         if filename.endswith('.py'):
             extension = 'cogs.' + filename[:-3]
-            print(f"{extension} 모듈을 불러왔습니다.")
             await bot.load_extension(extension)
+            print(f"{extension} 모듈을 불러왔습니다.")
 
 @bot.event
 async def on_ready():
